e82b559d8027fc452cca08f604937d7d8e1fae7f
msladic1 <sladic.muris@gmail.com>
2023-06-19 14:36:34 +0200

    Creating the form

--- app.py
+++ app.py
@@ -50,12 +50,7 @@ def prompt_to_palette():
 
 @app.route("/")
 def index():
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Give me a funny word: "
-    )
-    return response["choices"][0]["text"]
-    # return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
